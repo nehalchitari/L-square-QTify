d42ac604dfb930ac6f74c84a48d5eea48761ebcc
Nehal <nehalchitari@gmail.com>
2023-12-20 17:26:58 +0530

    Navbar_implementation

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-//import {ReactComponent as Logoo} from "../../assets/qtify_logo.svg";
 import LogoImage from "../../assets/Logo.png";
 
-
-
-export default function Logo(){
-    return <img src={LogoImage} alt="logo" width={67}/>
+export default function Logo() {
+  return <img src={LogoImage} alt="logo" width={67} />;
 }
